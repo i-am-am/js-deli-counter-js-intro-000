c5e212496bcfaba484502270dc7bd866ab9146a8
Alena <i.am.am.629@gmail.com>
2018-08-01 17:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(line, name){
 }
 
 function nowServing(line){
-  if line.length > 0 {
+  if (line.length > 0) {
     var i = line.shift()
     return `Currently serving ${i}.`
   } else {
@@ -12,13 +12,13 @@ function nowServing(line){
 }
 
 function currentLine(line){
-  if line.length = 0 {
+  if (line.length = 0) {
     return "The line is currently empty."
   }
   var sline = `The line is currently:`
   for (i = 0, i < line.length, i++){
     sline += ` ${i + 1}. ${line[i]}`
-    if i != line.length {
+    if (i != line.length) {
       sline += `,`
     }
   }
