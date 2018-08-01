442b4d0d7c258a2da47f7511a7294b7d7a03e4af
Alena <i.am.am.629@gmail.com>
2018-08-01 17:46:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,9 @@ function currentLine(line){
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    var sline = `The line is currently:`
-    for (var i = 0; i < line.length; i++){
-      sline += ` ${i + 1}. ${line[i]}`
-      if (i != line.length) {
-        sline += `,`
-      }
+    var sline = `The line is currently: 1. ${line[0]}`
+    for (var i = 1; i < line.length; i++){
+      sline += `, ${i + 1}. ${line[i]}`
     }
     return sline
   }
